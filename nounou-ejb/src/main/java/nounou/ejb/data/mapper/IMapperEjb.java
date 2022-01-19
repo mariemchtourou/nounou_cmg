580896ddd5d4f2609e5cb7d4e29f468e2bc75ce0
meriam mhadhbi <meriam.mhadhebi@esprit.tn>
2022-01-19 10:22:54 +0100

    IMapper

--- nounou-ejb/src/main/java/nounou/ejb/data/mapper/IMapperEjb.java
+++ nounou-ejb/src/main/java/nounou/ejb/data/mapper/IMapperEjb.java
@@ -1,94 +1,96 @@
 package nounou.ejb.data.mapper;
 
+
+
 import org.mapstruct.AfterMapping;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
 import org.mapstruct.factory.Mappers;
 
+
+
 import nounou.commun.dto.DtoCategorie;
 import nounou.commun.dto.DtoCompte;
-<<<<<<< HEAD
 import nounou.commun.dto.DtoContrat;
-=======
 import nounou.commun.dto.DtoParent;
->>>>>>> branch 'master' of https://github.com/mariemchtourou/nounou_cmg.git
 import nounou.commun.dto.DtoPersonne;
 import nounou.commun.dto.DtoTelephone;
 import nounou.ejb.data.Categorie;
 import nounou.ejb.data.Compte;
-<<<<<<< HEAD
 import nounou.ejb.data.Contrat;
-=======
 import nounou.ejb.data.Parent;
->>>>>>> branch 'master' of https://github.com/mariemchtourou/nounou_cmg.git
 import nounou.ejb.data.Personne;
 import nounou.ejb.data.Telephone;
 
- 
+
+
+
 @Mapper( componentModel = "cdi" )
-public interface IMapperEjb {  
-	
-	static final IMapperEjb INSTANCE = Mappers.getMapper( IMapperEjb.class );
-	
-	//Parent
-	
-	Parent map( DtoParent Parent );
-	
-	DtoParent map( Parent source ); 
-	
-	
-	// Compte
-	
-	Compte map( DtoCompte source );
-	
-	DtoCompte map( Compte source );
+public interface IMapperEjb {
 
-	
-	// Categorie
-	
-	Categorie map( DtoCategorie source );
-	
-	DtoCategorie map( Categorie source );
+static final IMapperEjb INSTANCE = Mappers.getMapper( IMapperEjb.class );
 
-	
-	// Personne
-	
-	Personne map( DtoPersonne source );
-	
-	DtoPersonne map( Personne source );
+//Parent
 
-	@Mapping( target="categorie", ignore = true )
-	@Mapping( target="telephones", ignore = true )
-	DtoPersonne mapMinimal( Personne source );
-	
-<<<<<<< HEAD
-	//Contrat
-	
-	Contrat map( DtoContrat source );
-	
-	DtoContrat map( Contrat source);
-	
-	@Mapping( target="IdParent", ignore = true )
-	DtoContrat mapMinimal( Contrat source );
-	
-=======
->>>>>>> branch 'master' of https://github.com/mariemchtourou/nounou_cmg.git
-	// Telephone
-	
-	@Mapping( target="personne", ignore=true )
-	Telephone map( DtoTelephone source );
-	
-	DtoTelephone map( Telephone source );
-	
+Parent map( DtoParent Parent );
 
-	// Méthodes auxiliaires
-	
-    @AfterMapping
-    public default void addBackReference(@MappingTarget Personne target) {
-        for (Telephone telephone : target.getTelephones() ) {
-        	telephone.setPersonne( target );
-        }
-    }	
-	
+DtoParent map( Parent source );
+
+// Compte
+
+Compte map( DtoCompte source );
+
+DtoCompte map( Compte source );
+
+
+
+
+// Categorie
+
+Categorie map( DtoCategorie source );
+
+DtoCategorie map( Categorie source );
+
+
+
+
+// Personne
+
+Personne map( DtoPersonne source );
+
+DtoPersonne map( Personne source );
+
+
+
+@Mapping( target="categorie", ignore = true )
+@Mapping( target="telephones", ignore = true )
+DtoPersonne mapMinimal( Personne source );
+
+//Contrat
+
+Contrat map( DtoContrat source );
+
+DtoContrat map( Contrat source);
+
+
+
+// Telephone
+
+@Mapping( target="personne", ignore=true )
+Telephone map( DtoTelephone source );
+
+DtoTelephone map( Telephone source );
+
+
+
+// Méthodes auxiliaires
+
+@AfterMapping
+public default void addBackReference(@MappingTarget Personne target) {
+for (Telephone telephone : target.getTelephones() ) {
+telephone.setPersonne( target );
+}
+}
+
 }
